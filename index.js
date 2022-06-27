5885c3fde4f6a018210cb7a5a4f8b97e96a8b02e
Marek Radiměřský <xradimer@fi.muni.cz>
2022-06-27 11:22:02 +0200

    Create signature endpoint

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const hash = require("object-hash");
+var crypto = require("crypto");
+var fs = require("fs");
 
 const app = express();
 
 // create application/json parser
-var jsonParser = bodyParser.json()
+var jsonParser = bodyParser.json();
 
-app.get('/', (req, res) => {
-  res.send('<h1>Home page</h1>');
+app.get("/", (req, res) => {
+  res.send("<h1>Server is running</h1>");
 });
 
-app.post('/sign', jsonParser, (req, res) => {
-  const {name} = req.body;
-  console.log(req.body);
-  if (!name) {
-    res.status(400).send({ message: "Please add name of user"});
+app.post("/sign", jsonParser, (req, res) => {
+  const data = req.body;
+
+  if (!data) {
+    res.status(400).send({ message: "Please provide data to hash" });
   }
-  res.send({ name: name, id: 3});
+
+  const dataHash = hash(data);
+  const hashSignature = signHash(dataHash)
+
+  res.send({ hash: dataHash, signature: hashSignature });
 });
 
 app.listen(3001, () => {
-  console.log('Server started on port 3001.');
-  console.log('Local server: http://localhost:3001/');
+  console.log("Server started on port 3001.");
+  console.log("Local server: http://localhost:3001/");
 });
+
+const signHash = (hash) => {
+  // Read private key from file
+  const pem = fs.readFileSync("./keys/key");
+  const key = pem.toString("ascii");
+
+  var sign = crypto.createSign("RSA-SHA256");
+  sign.update(hash); // data from your file would go here
+
+  var sig = sign.sign(key, "hex");
+  return sig;
+};
